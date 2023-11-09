bf83685bb4f243137b32cb6d89b3611cbaa889b3
Simon <saimeunt@gmail.com>
2023-11-09 21:08:02 +0100

    wip

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -20,7 +20,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const initialState = { message: null, errors: {} };
+  const initialState = { message: "", errors: {} };
   const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
   return (
     <form action={dispatch}>
